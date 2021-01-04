2a5eadef809445575945b0561f284aa7a2405706
Ben Smith <ben@ninjatech.com>
2021-01-03 20:25:03 -0800

    write a couple of tests for the CORS setup and learn some pytest in the process. Add an origins parameter to setup_cors()

--- app/cors.py
+++ app/cors.py
@@ -1,20 +1,25 @@
+from typing import List, Optional
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 # TODO: better if these parameters are not hardcoded
-
-origins = [
+default_origins = [
     "http://localhost",
     "http://localhost:8080",
 ]
 
 
-def setup_cors(app: FastAPI) -> None:
+def setup_cors(app: FastAPI, origins: Optional[List[str]] = None) -> None:
     """
     Configures CORS
     :param app: the FastAPI app instance
+    :param origins: the allowed origins for CORS
     :return: None
     """
+    if origins is None:
+        origins = default_origins
+
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
--- tests/unit/test_jwtbearer.py
+++ tests/unit/test_jwtbearer.py
@@ -1,10 +1,5 @@
 import app.jwtbearer
 
-def test_true():
-    assert True
-
-def test_false():
-    assert False
 
 class TestGetJWKS:
     ...
